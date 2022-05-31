59c26e6c34bdbd83820e0780d72d7e24ccf0067a
moreanonymousbecauseiammysterious <87724830+moreanonymousbecauseiammysterious@users.noreply.github.com>
2022-05-31 16:43:20 -0400

    Add files via upload

---
+++ dont touch this folder/BellCurve.py
@@ -0,0 +1,7 @@
+import pandas as pd
+import csv
+import plotly.figure_factory as ff
+
+df = pd.read_csv("control s.csv")
+fig = ff.create_distplot([df["Height(Inches)"].tolist()],["Height"],show_hist = False)
+fig.show()
